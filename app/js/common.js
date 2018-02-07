95e9a59e04a4122d96ce8d4b469124eb1e39a9f5
Andrey E <akharitonov40px@gmail.com>
2018-02-07 12:04:54 +0500

    fix

--- app/js/common.js
+++ app/js/common.js
@@ -20,8 +20,20 @@ $(document).ready(function(){
   $(".slider-partners").owlCarousel({
     items: 4,
     loop: true,
-    dots: true,
-    dotsEach: true,
+    nav: true,
+    dots: false,
+    dotsEach: false,
     navText: ""
+  });
+
+  $(window).scroll(function() {
+  if($(this).scrollTop() != 0) {
+  $('#toTop').fadeIn();
+  } else {
+  $('#toTop').fadeOut();
+  }
+  });
+  $('#toTop').click(function() {
+   $('body,html').animate({scrollTop:0},800);
   });
 });
